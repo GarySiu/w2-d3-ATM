b96d4d2e1e5c4fdb18a328b84ebb7eed0d541952
Gary Siu <Gary@untypeable.com>
2015-08-19 23:42:25 +0100

    Deposit function with basic validation

--- atm.js
+++ atm.js
@@ -1,4 +1,35 @@
-$(document).ready(function(){
-  
-})
+var atm = atm || {}; //Gotta learn to namespace some time
 
+atm.deposit1 = function() {
+ //WARNING: HERE BE BRACKETS. Test input for non-numbers
+  if(isNaN(parseInt($('#amount1').val()))) {
+    $('#amount1').val('');
+    return;
+  }
+  var balance = parseInt($('#balance1').text().split('$')[1]);
+  balance = balance + parseInt($('#amount1').val());
+  $('#balance1').text('$'+balance);
+  $('#amount1').val('');
+}
+atm.deposit2 = function() {
+  $('#balance2').text('$'+$('#amount2').val());
+  $('#amount2').val('');
+}
+atm.withdraw1 = function() {
+  var balance = parseInt($('#balance1').text().split('$')[1])
+  balance = balance - $('#amount1').val();
+  $('#balance1').text('$'+balance);
+  $('#amount1').val('');
+}
+atm.validDeposit1 = function() {
+  }
+
+atm.setupListeners = function() {
+  $('#deposit1').on('click', atm.deposit1);
+  $('#deposit2').on('click', atm.deposit2);
+  $('#withdraw1').on('click', atm.withdraw1);
+}
+
+$(document).ready(function(){
+  atm.setupListeners();
+})
