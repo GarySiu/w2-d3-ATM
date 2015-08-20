d72790d14cb54e83529d36944237f6264554a9ad
Gary Siu <Gary@untypeable.com>
2015-08-20 07:32:05 +0100

    Naive withdraw, no protection

--- atm.js
+++ atm.js
@@ -12,22 +12,40 @@ atm.deposit1 = function() {
   $('#amount1').val('');
 }
 atm.deposit2 = function() {
-  $('#balance2').text('$'+$('#amount2').val());
+  if(isNaN(parseInt($('#amount2').val()))) {
+    $('#amount2').val('');
+    return;
+  }
+  var balance = parseInt($('#balance2').text().split('$')[1]);
+  balance = balance + parseInt($('#amount2').val());
+  $('#balance2').text('$'+balance);
   $('#amount2').val('');
 }
 atm.withdraw1 = function() {
-  var balance = parseInt($('#balance1').text().split('$')[1])
-  balance = balance - $('#amount1').val();
+  if(isNaN(parseInt($('#amount1').val()))) {
+    $('#amount1').val('');
+    return;
+  }
+  var balance = parseInt($('#balance1').text().split('$')[1]);
+  balance = balance - parseInt($('#amount1').val());
   $('#balance1').text('$'+balance);
   $('#amount1').val('');
 }
-atm.validDeposit1 = function() {
+atm.withdraw2 = function() {
+  if(isNaN(parseInt($('#amount2').val()))) {
+    $('#amount2').val('');
+    return;
   }
-
+  var balance = parseInt($('#balance2').text().split('$')[1]);
+  balance = balance - parseInt($('#amount2').val());
+  $('#balance2').text('$'+balance);
+  $('#amount2').val('');
+}
 atm.setupListeners = function() {
   $('#deposit1').on('click', atm.deposit1);
   $('#deposit2').on('click', atm.deposit2);
   $('#withdraw1').on('click', atm.withdraw1);
+  $('#withdraw2').on('click', atm.withdraw2);
 }
 
 $(document).ready(function(){
